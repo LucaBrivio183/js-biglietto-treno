77d88abbe0dbb2fa6a265792fcdb3a96cac45173
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-21 14:40:50 +0100

    feat: defined variables prices for age categories

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 
 'use strict'
 
+//define standard price for Km
+
+let standardKmPrice = 0.21;
+
+//define 20% discounted price for Km for minors
+
+let discountMinors = 20/100;
+
+let minorsKmPrice = standardKmPrice * (1 - discountMinors);
+
+//define 40% discounted price for Km for over 65
+
+let discountOld = 40/100;
+
+let oldKmPrice = standardKmPrice * (1 - discountOld);
+
+
 
 
 
